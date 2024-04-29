81c1594335c822528827400a2553dbba251b965f
Mani Kanta <konugantimanikantareddy@gmail.com>
2024-04-29 10:22:04 +0530

    Added text

--- 01-word-count.sh
+++ 01-word-count.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-# Open the text file in read mode
-text_file="count.txt"
-cat $text_file |
+# # Open the text file in read mode
+# text_file="count.txt"
+# cat $text_file |
 
-# Split the text file into words
-tr '[:space:]' '\n' |
+# # Split the text file into words
+# tr '[:space:]' '\n' |
 
-# Count the frequency of each word
-sort | uniq -c |
+# # Count the frequency of each word
+# sort | uniq -c |
 
-# Sort the words by frequency
-sort -rn |
+# # Sort the words by frequency
+# sort -rn |
 
-# Print the most frequent word
-head -n 1
+# # Print the most frequent word
+# head -n 1
+
+
+sed -e 's/[^[:alpha:]]/ /g' count.txt | tr '\n' " " |  tr -s " " | tr " " '\n'| tr 'A-Z' 'a-z' | sort | uniq -c | sort -nr | nl 
